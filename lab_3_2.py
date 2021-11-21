f38e88df764b11a17fdc24e9a4f98d00f0e2a8a0
Tornad00 <91788538+Tornad00@users.noreply.github.com>
2021-11-21 19:19:59 +0000

    l

--- lab_3_2.py
+++ lab_3_2.py
@@ -5,8 +5,11 @@ h = 100
 a = co.PI / 3
 B = 0.52
 g = co.g
+T = 200 
+eps = 300
 
 v = np.sqrt( (h * g * np.tan(B)**2) / (2 * np.cos(a)**2 * (1- np.tan(B) * np.tan(a))) )
 print(v)
 
-N
+n=(2/np.sqrt(co.PI))*(co.h/np.sqrt((co.k*T)**(3)))*(eps**(-eps/(co.k*T)))*(eps**(T/2))
+print(n)
