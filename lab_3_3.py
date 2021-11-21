c176814b7d82ce2dea9fd9e7238553db78fd3dba
Tornad00 <91788538+Tornad00@users.noreply.github.com>
2021-11-21 19:30:19 +0000

    h

--- lab_3_3.py
+++ lab_3_3.py
@@ -4,7 +4,7 @@ import const as co
 x = float(input("введите значение X0 = "))
 y = float(input("введите значение Y0 = "))
 v = float(input("введите значение V0 = "))
-t = np.arange(0, 6, 1)
 
-while t == 5:
+
+while t :
   x = x + v * t
